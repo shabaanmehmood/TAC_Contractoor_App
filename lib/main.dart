a76eff5b317cbc9a5869402df65c73e04cb37b6a
Hamza-Pervaiz <129182457+Hamza-Pervaiz@users.noreply.github.com>
2025-11-06 20:16:32 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -324,12 +324,6 @@ import 'modules/Jobs/Create Jobs/company_info_controller.dart';
 //   }
 // }
 
-
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:local_auth/local_auth.dart';
